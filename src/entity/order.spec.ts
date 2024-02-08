47c44ca4e40646d0f7417fc555d555fc73096858
Luis <luiscvlh11@gmail.com>
2024-02-07 21:30:28 -0300

    test(tests): :white_check_mark: atualiza os testes de order

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -21,19 +21,26 @@ describe("Order unit tests", () => {
     });
 
     it("should calculate total", () => {
-        const item1 = new OrderItem("123", "Item 1", 10);
-        const item2 = new OrderItem("1234", "Item 2", 10);
+        const item1 = new OrderItem("123", "Item 1", 10, "123", 2);
+        const item2 = new OrderItem("1234", "Item 2", 10, "123", 2);
 
         const order1 = new Order("123", "123", [item1]);
 
         let total = order1.total();
 
-        expect(total).toBe(10);
+        expect(total).toBe(20);
 
         const order2 = new Order("1234", "1234", [item1, item2]);
 
         total = order2.total();
 
-        expect(total).toBe(20);
+        expect(total).toBe(40);
+    });
+
+    it("should throw error if the item quantity is less or equal to zero ", () => {
+        expect(() => {
+            const item1 = new OrderItem("123", "Item 1", 10, "123", 0);
+            const order = new Order("123", "123", [item1]);
+        }).toThrow("Quantity must be greater than 0");
     });
 });

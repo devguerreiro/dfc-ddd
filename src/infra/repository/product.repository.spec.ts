29fe800222e5fcb2da8a17dc052512927e727432
Luis <luiscvlh11@gmail.com>
2024-02-20 21:22:48 -0300

    feat(repository): :sparkles: implementa o crud de produtos com testes unitarios

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
+import ProductRepository from "./product.repository";
 
 describe("Product repository test", () => {
     let sequelize: Sequelize;
@@ -18,5 +20,81 @@ describe("Product repository test", () => {
 
     afterEach(async () => {
         await sequelize.close();
+    });
+
+    it("should create a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Produto", 100);
+
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+        expect(productModel.toJSON()).toStrictEqual({
+            id: "1",
+            name: "Produto",
+            price: 100,
+        });
+    });
+
+    it("should update a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Produto", 100);
+
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+        expect(productModel.toJSON()).toStrictEqual({
+            id: "1",
+            name: "Produto",
+            price: 100,
+        });
+
+        product.changeName("Produto Atualizado");
+        product.changePrice(200);
+
+        await productRepository.update(product);
+
+        const productModelUpdated = await ProductModel.findOne({
+            where: { id: "1" },
+        });
+
+        expect(productModelUpdated.toJSON()).toStrictEqual({
+            id: "1",
+            name: "Produto Atualizado",
+            price: 200,
+        });
+    });
+
+    it("should find a product by id", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Produto", 100);
+
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+        const foundProduct = await productRepository.find("1");
+
+        expect(productModel.toJSON()).toStrictEqual({
+            id: foundProduct.id,
+            name: foundProduct.name,
+            price: foundProduct.price,
+        });
+    });
+
+    it("should find all products", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Produto", 100);
+        await productRepository.create(product);
+
+        const product2 = new Product("2", "Produto", 200);
+        await productRepository.create(product2);
+
+        const foundProducts = await productRepository.findAll();
+        const products = [product, product2];
+
+        expect(foundProducts).toEqual(products);
     });
 });
